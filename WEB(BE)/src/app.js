23dbdbd064b16cf38cb81b3896b514e1bfaf5fbe
YoungJune Kang <cerealmaster@naver.com>
2022-09-19 14:10:04 +0000

    dotenv setting

--- WEB(BE)/src/app.js
+++ WEB(BE)/src/app.js
@@ -1,10 +1,26 @@
-// const express = require('express');
-
 // es6 way
 import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+//env setting
+import "./env.js";
+import { db_cstring } from "./db.js";
+console.log(db_cstring);
+
+const localPort = 5000;
 
 const app = express();
 
-app.listen(5000,()=>{
-    console.log("listening on port 5000");
-});
+//middlewares
+app.use(bodyParser.json());
+
+
+//mongoose connection
+mongoose
+    .connect("mongodb+srv://WILIM:wilimadmin@wilim-database.erisix0.mongodb.net/?retryWrites=true&w=majority")
+    .then(()=>{
+        app.listen(localPort,()=>{
+            console.log(`listening on ${localPort}`);
+        });
+    })
+    .catch((e)=>console.log(e));
