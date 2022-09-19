590db5c9b21777d226579a75664d20801d31c8ce
leeji7682 <leeji7682@gmail.com>
2022-09-19 14:11:47 +0000

    text 컴포넌트

---
+++ WEB(FE)/wilim-front/src/components/atom/text.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+import { BaseStyles } from '../theme';
+
+export const Text = ({ color, fontSize, fontFamily, textAlign }) => {
+    return (
+        <TextDiv color={color} font-size={fontSize} font-family={fontFamily} text-align={textAlign} />
+    );
+};
+
+const TextDiv = styled.text`
+    color: ${({color}) => color || 'black'};
+    font-size: ${({fontSize}) => fontSize || '10px'};
+    font-weight: ${({fontWeight}) => fontWeight || '1.2'};
+    font-family: ${({fontFamily}) => fontFamily || 'Inter'};
+    text-align: ${({textAlign}) => textAlign || 'left'};
+`;
