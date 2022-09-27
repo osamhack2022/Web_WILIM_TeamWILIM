fa1620c4c5f180674f1eacffa729059c13e2ae21
Geun-Oh <kandy1002@naver.com>
2022-09-27 13:32:50 +0000

    goal&plan page updated

--- WEB(FE)/wilim-front/src/components/template/goalPlanTemplate.js
+++ WEB(FE)/wilim-front/src/components/template/goalPlanTemplate.js
@@ -8,7 +8,7 @@ import { BaseStyles } from "../theme";
 import { useSelector } from "react-redux";
 
 export const GoalPlanTemplate = () => {
-  const username = useSelector(state => state.userInfo.username);
+  const username = useSelector((state) => state.userInfo.username);
   return (
     <Flex flexDirection="column" alignItems="center">
       <Flex flexDirection="column" alignItems="center" width="80%">
@@ -28,8 +28,40 @@ export const GoalPlanTemplate = () => {
         />
       </Flex>
       <MarginBox marginBottom="2rem" />
+      <Flex
+        flexDirection="column"
+        justifyContent="flex-start"
+        width="calc(80% + 2rem)"
+      >
+        <Text
+          innerText="Goal"
+          color="white"
+          fontSize={BaseStyles.Text.Heading2}
+          fontWeight={BaseStyles.Text.Border0}
+        />
+      </Flex>
+      <MarginBox marginBottom="1rem" />
       <GoalCard />
       <MarginBox marginBottom="2rem" />
+      <Line
+          width="30%"
+          height="1px"
+          color={BaseStyles.Color.Black2}
+        />
+      <MarginBox marginBottom="2rem" />
+      <Flex
+        flexDirection="column"
+        justifyContent="flex-start"
+        width="calc(80% + 2rem)"
+      >
+        <Text
+          innerText="Plan"
+          color="white"
+          fontSize={BaseStyles.Text.Heading2}
+          fontWeight={BaseStyles.Text.Border0}
+        />
+      </Flex>
+      <MarginBox marginBottom="1rem" />
       <PlanCard />
     </Flex>
   );
