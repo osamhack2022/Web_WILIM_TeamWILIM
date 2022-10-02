b6c1f7598b6ff52726487bb857d86c55fab13ed7
leeji7682 <leeji7682@gmail.com>
2022-10-02 13:48:08 +0000

    Apply route

--- WEB(FE)/ts-wilim/src/App.tsx
+++ WEB(FE)/ts-wilim/src/App.tsx
@@ -1,8 +1,16 @@
 import { Layout } from "./layout/Layout";
+import { Route, Routes } from "react-router-dom";
+import { LoginPage } from "./pages/loginPage";
+import { CreateAccountPage } from "./pages/creatAccountPage";
 
 function App() {
   return (
-    <Layout />
+    //<Layout />
+    
+    <Routes>
+      <Route path="/" element={<LoginPage />}/>
+      <Route path="/CreateAccountPage" element={<CreateAccountPage />}/>
+    </Routes>
   );
 }
 
