ef366ed7c1807044cafcc963d1ac948cacfc20cb
YoungJune Kang <cerealmaster@naver.com>
2022-10-02 14:53:53 +0000

    package.json dependencies/devDependencies seperated

--- WEB(BE)/src/controller/newUserInfoFetchingAPI.js
+++ WEB(BE)/src/controller/newUserInfoFetchingAPI.js
@@ -1,5 +1,6 @@
 import User from '../models/user';
 import axios from 'axios';
+// https 로 데이터 받아오면 validation 에서 막히는듯하다. url 은 왠만하면 http 로 받아오자...!
 // process.env.NODE_TLS_REJECT_UNAUTHORIZED ="0";
 
 module.exports.getFetchWithQnet = async(req,res,next)=>{
