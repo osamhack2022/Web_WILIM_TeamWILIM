e7c3c922fc3288425d65a4e6fcc7cad9cc70b0ee
Geun-Oh <kandy1002@naver.com>
2022-10-04 13:50:44 +0000

    테스트페이지 이동 버튼 제작

--- WEB(FE)/ts-wilim/src/App.tsx
+++ WEB(FE)/ts-wilim/src/App.tsx
@@ -6,12 +6,19 @@ import { GoalPlanPage } from "./pages/goalPlanPage";
 import { RefCommunityPage } from "./pages/refCommunityPage";
 import { FindPasswordPage } from "./pages/findPasswordPage";
 import { UserSchemaPage } from "./pages/userSchemaPage";
+import { Button } from "./components/atom/button";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LoginPage />}/>
-      <Route path="/createAccount" element={<CreateAccountPage />}/>
+      <Route path="/" element={<>
+        <LoginPage />
+        <Link to="/test">
+          <Button innerText="테스트페이지로 이동" onClick={() => console.log("테스트페이지로 이동")} />
+        </Link>
+      </>} />
+      <Route path="/createAccount" element={<CreateAccountPage />} />
       <Route path="/goalPlan/:username" element={<GoalPlanPage />} />
       <Route path="/refCommunity/:username" element={<RefCommunityPage />} />
       <Route path="/findPassword" element={<FindPasswordPage />} />
