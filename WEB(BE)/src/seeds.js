f292aa201e22a383dc2db6ddc668bd56dfaf7674
YoungJune Kang <cerealmaster@naver.com>
2022-10-05 15:01:29 +0000

    backend - seeds.js testDate fetch(failed)

--- WEB(BE)/src/seeds.js
+++ WEB(BE)/src/seeds.js
@@ -1,5 +1,4 @@
 //개발용 dummy data 심는 파일
-//id, username, password, email, goal, plan
 import mongoose from 'mongoose';
 import User from './models/user.js';
 import GoalElement from './models/goalElement';
@@ -8,16 +7,17 @@ import {db_cstring, qnet_key} from "./db.js";
 import axios from 'axios';
 import express from 'express';
 import qnetInfo from './qnetInfo.json';
+import fs from 'fs';
 
 const app = express();
 
 
 //mongoose connection
-main().catch(err => console.log(err));
-async function main() {
-    await mongoose.connect(db_cstring);
-    console.log("database connected!");
-}
+// main().catch(err => console.log(err));
+// async function main() {
+//     await mongoose.connect(db_cstring);
+//     console.log("database connected!");
+// }
 
 function fillZero(str){
     return str.length >= 4 ? str:new Array(4-str.length+1).join('0')+str;
@@ -40,43 +40,75 @@ const dataFormat = 'json';
 let implYy = 2022;
 const length = items.length;//시험 종목 갯수
 let obj = [];
+let cnt = 0;
+const seedDB = async() =>{
+    for await (let item of items){
+        const response = await axios.get(`${url}?serviceKey=${serviceKey}&numOfRows=${numOfRows}&pageNo=${pageNo}&dataFormat=${dataFormat}&implYy=${implYy}&qualgbCd=${item.qualgbcd}&jmCd=${fillZero(String(item.jmcd))}`);
+        const totalCount = response.data.body.totalCount;
+        let objInsideObj = [];
+        for(let i = 0; i < totalCount; i++){
+            const data = {
+                implYy : response.data.body.items[i].implYy,
+                implseq : response.data.body.items[i].implSeq,
+                dateDescription : response.data.body.items[i].description,
+                docRegStartDt : response.data.body.items[i].docRegStartDt,
+                docRegEndDt : response.data.body.items[i].docRegEndDt,
+                docExamStartDt : response.data.body.items[i].docExamStartDt,
+                docExamEndDt : response.data.body.items[i].docExamEndDt,
+                docPassDt : response.data.body.items[i].docPassDt,
+                pracRegStartDt : response.data.body.items[i].pracRegStartDt,
+                pracRegEndDt : response.data.body.items[i].pracRegEndDt,
+                pracExamStartDt : response.data.body.items[i].pracExamStartDt,
+                pracExamEndDt : response.data.body.items[i].pracExamEndDt,
+                pracPassDt : response.data.body.items[i].pracPassDt
+            };
+            objInsideObj.push(data);
+        } 
+        obj.push(objInsideObj);
+    }
+}
 
+seedDB()
+    .then(()=>{
+        const dictstring = JSON.stringify(obj);
+        fs.writeFile('qnetTestDate.json',dictstring);
+    }).catch((e)=>console.log(e))
 
 // console.log(`${url}?serviceKey=${qnet_key}&numOfRows=${numOfRows}&pageNo=${pageNo}&dataFormat=${dataFormat}&implYy=${implYy}&qualgbCd=${item[0].qualgbcd}&jmCd=${jmCd}`);
 // res.data.body.items
-const seedDB = ((item)=>{
-    axios.get(`${url}?serviceKey=${serviceKey}&numOfRows=${numOfRows}&pageNo=${pageNo}&dataFormat=${dataFormat}&implYy=${implYy}&qualgbCd=${item.qualgbcd}&jmCd=${fillZero((item.jmcd).toString())}`)
-    .then(async (res)=>{
-        const data = await res.data.body.items;
-        const dataJson = {
-            name : item.jmfldnm,
-            qualgbnm : item.qualgbnm,
-            description : item.description,
-            seriesnm : item.seriesnm,
-            obligfldnm : item.obligfldnm,
-            mdobligfldnm : item.mdobligfldnm,
-            implYy : data.implYy,
-            implseq : data.implSeq,
-            dateDescription : data.dateDescription,
-            docRegStartDt : data.docRegStartDt,
-            docRegEndDt : data.docRegEndDt,
-            docExamStartDt : data.docExamStartDt,
-            docExamEndDt : data.docExamEndDt,
-            docPassDt : data.docPassDt,
-            pracRegStartDt : data.pracRegStartDt,
-            pracRegEndDt : data.pracRegEndDt,
-            pracExamStartDt : data.pracExamStartDt,
-            pracExamEndDt : data.pracExamEndDt,
-            pracPassDt : data.pracPassDt
-        }
-        const newData = new GoalElement(dataJson);
-        await newData.save();
-    }).catch((e)=>console.log(e))
-})
+// const seedDB = ((item)=>{
+//     axios.get(`${url}?serviceKey=${serviceKey}&numOfRows=${numOfRows}&pageNo=${pageNo}&dataFormat=${dataFormat}&implYy=${implYy}&qualgbCd=${item.qualgbcd}&jmCd=${fillZero((item.jmcd).toString())}`)
+//     .then(async (res)=>{
+//         const data = await res.data.body.items;
+//         const dataJson = {
+//             name : item.jmfldnm,
+//             qualgbnm : item.qualgbnm,
+//             description : item.description,
+//             seriesnm : item.seriesnm,
+//             obligfldnm : item.obligfldnm,
+//             mdobligfldnm : item.mdobligfldnm,
+//             implYy : data.implYy,
+//             implseq : data.implSeq,
+//             dateDescription : data.dateDescription,
+//             docRegStartDt : data.docRegStartDt,
+//             docRegEndDt : data.docRegEndDt,
+//             docExamStartDt : data.docExamStartDt,
+//             docExamEndDt : data.docExamEndDt,
+//             docPassDt : data.docPassDt,
+//             pracRegStartDt : data.pracRegStartDt,
+//             pracRegEndDt : data.pracRegEndDt,
+//             pracExamStartDt : data.pracExamStartDt,
+//             pracExamEndDt : data.pracExamEndDt,
+//             pracPassDt : data.pracPassDt
+//         }
+//         const newData = new GoalElement(dataJson);
+//         await newData.save();
+//     }).catch((e)=>console.log(e))
+// })
 
-for await (const item of items){
-    seedDB(item);
-}
+// for await (const item of items){
+//     seedDB(item);
+// }
 // const seedDB = async () =>{
 //     await User.deleteMany({});
 //     await User.insertMany(data);
