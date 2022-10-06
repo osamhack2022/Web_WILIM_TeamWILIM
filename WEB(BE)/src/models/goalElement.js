f5a976468a2b91960b3c8cb68bd3e52cade4bc82
YoungJune Kang <cerealmaster@naver.com>
2022-10-06 13:51:19 +0000

    backend - goalEleement model modified, DB seeded

--- WEB(BE)/src/models/goalElement.js
+++ WEB(BE)/src/models/goalElement.js
@@ -7,19 +7,22 @@ const goalElementSchema = new mongoose.Schema({
     seriesnm : {type : String}, //계열명 ex 기술사
     obligfldnm : {type : String}, //대직무분야명
     mdobligfldnm: {type : String}, //중직무분야명
-    implYy : {type : Number}, // 시행년도
-    implSeq : {type : Number}, //시행회차
-    dateDescription : {type : String},//시험 회차및 년도 설명
-    docRegStartDt : {type : Number},//필기시험 원서접수 시작일자
-    docRegEndDt : {type : Number}, //필기시험 원서접수 종료일자
-    docExamStartDt : {type : Number}, //필기시험 시작일자
-    docExamEndDt : {type : Number}, //필기시험 종료일자
-    docPassDt : {type : Number}, //필기시험 합격(예정)자 발표일자
-    pracRegStartDt : {type : Number},//실기(작업)/면접 시험 원서접수 시작일자
-    pracRegEndDt : {type : Number},  //실기(작업)/면접 시험 원서접수 종료일자
-    pracExamStartDt : {type : Number}, //실기(작업)/면접 시험 시작일자
-    pracExamEndDt : {type : Number}, //실기(작업)/면접 시험 종료일자
-    pracPassDt : {type : Number}, //실기(작업)/면접 합격자 발표일자
+    dateUrl : {type : String}, // 일정 api url
+    //<--------------------------------------------------------->
+    // implYy : {type : Number}, // 시행년도
+    // implSeq : {type : Number}, //시행회차
+    // dateDescription : {type : String},//시험 회차및 년도 설명
+    // docRegStartDt : {type : Number},//필기시험 원서접수 시작일자
+    // docRegEndDt : {type : Number}, //필기시험 원서접수 종료일자
+    // docExamStartDt : {type : Number}, //필기시험 시작일자
+    // docExamEndDt : {type : Number}, //필기시험 종료일자
+    // docPassDt : {type : Number}, //필기시험 합격(예정)자 발표일자
+    // pracRegStartDt : {type : Number},//실기(작업)/면접 시험 원서접수 시작일자
+    // pracRegEndDt : {type : Number},  //실기(작업)/면접 시험 원서접수 종료일자
+    // pracExamStartDt : {type : Number}, //실기(작업)/면접 시험 시작일자
+    // pracExamEndDt : {type : Number}, //실기(작업)/면접 시험 종료일자
+    // pracPassDt : {type : Number}, //실기(작업)/면접 합격자 발표일자
+    //<----------------------------------------------------------->
     mockLink : {type : String}, //공개문제 다운받는 링크
     isQnet : {type : Boolean}, // qnet 에서 가져온 자격증정보인지 /토익, 수능 등 외부에서 가져온 정보인지 (t/f)
     users : [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}], //이 자격증 목표로 한 유저들 id
--- WEB(BE)/src/seeds.js
+++ WEB(BE)/src/seeds.js
@@ -12,12 +12,12 @@ import fs from 'fs';
 const app = express();
 
 
-//mongoose connection
-// main().catch(err => console.log(err));
-// async function main() {
-//     await mongoose.connect(db_cstring);
-//     console.log("database connected!");
-// }
+mongoose.connection
+main().catch(err => console.log(err));
+async function main() {
+    await mongoose.connect(db_cstring);
+    console.log("database connected!");
+}
 
 function fillZero(str){
     return str.length >= 4 ? str:new Array(4-str.length+1).join('0')+str;
@@ -41,77 +41,56 @@ let implYy = 2022;
 const length = items.length;//시험 종목 갯수
 let obj = [];
 let cnt = 0;
-const seedDB = async() =>{
-    for await (let item of items){
-        const response = await axios.get(`${url}?serviceKey=${serviceKey}&numOfRows=${numOfRows}&pageNo=${pageNo}&dataFormat=${dataFormat}&implYy=${implYy}&qualgbCd=${item.qualgbcd}&jmCd=${fillZero(String(item.jmcd))}`);
-        const totalCount = response.data.body.totalCount;
-        let objInsideObj = [];
-        for(let i = 0; i < totalCount; i++){
-            const data = {
-                implYy : response.data.body.items[i].implYy,
-                implseq : response.data.body.items[i].implSeq,
-                dateDescription : response.data.body.items[i].description,
-                docRegStartDt : response.data.body.items[i].docRegStartDt,
-                docRegEndDt : response.data.body.items[i].docRegEndDt,
-                docExamStartDt : response.data.body.items[i].docExamStartDt,
-                docExamEndDt : response.data.body.items[i].docExamEndDt,
-                docPassDt : response.data.body.items[i].docPassDt,
-                pracRegStartDt : response.data.body.items[i].pracRegStartDt,
-                pracRegEndDt : response.data.body.items[i].pracRegEndDt,
-                pracExamStartDt : response.data.body.items[i].pracExamStartDt,
-                pracExamEndDt : response.data.body.items[i].pracExamEndDt,
-                pracPassDt : response.data.body.items[i].pracPassDt
-            };
-            objInsideObj.push(data);
-        } 
-        obj.push(objInsideObj);
-    }
-}
 
-seedDB()
-    .then(()=>{
-        const dictstring = JSON.stringify(obj);
-        fs.writeFile('qnetTestDate.json',dictstring);
-    }).catch((e)=>console.log(e))
-
-// console.log(`${url}?serviceKey=${qnet_key}&numOfRows=${numOfRows}&pageNo=${pageNo}&dataFormat=${dataFormat}&implYy=${implYy}&qualgbCd=${item[0].qualgbcd}&jmCd=${jmCd}`);
-// res.data.body.items
-// const seedDB = ((item)=>{
-//     axios.get(`${url}?serviceKey=${serviceKey}&numOfRows=${numOfRows}&pageNo=${pageNo}&dataFormat=${dataFormat}&implYy=${implYy}&qualgbCd=${item.qualgbcd}&jmCd=${fillZero((item.jmcd).toString())}`)
-//     .then(async (res)=>{
-//         const data = await res.data.body.items;
-//         const dataJson = {
-//             name : item.jmfldnm,
-//             qualgbnm : item.qualgbnm,
-//             description : item.description,
-//             seriesnm : item.seriesnm,
-//             obligfldnm : item.obligfldnm,
-//             mdobligfldnm : item.mdobligfldnm,
-//             implYy : data.implYy,
-//             implseq : data.implSeq,
-//             dateDescription : data.dateDescription,
-//             docRegStartDt : data.docRegStartDt,
-//             docRegEndDt : data.docRegEndDt,
-//             docExamStartDt : data.docExamStartDt,
-//             docExamEndDt : data.docExamEndDt,
-//             docPassDt : data.docPassDt,
-//             pracRegStartDt : data.pracRegStartDt,
-//             pracRegEndDt : data.pracRegEndDt,
-//             pracExamStartDt : data.pracExamStartDt,
-//             pracExamEndDt : data.pracExamEndDt,
-//             pracPassDt : data.pracPassDt
-//         }
-//         const newData = new GoalElement(dataJson);
-//         await newData.save();
-//     }).catch((e)=>console.log(e))
-// })
-
-// for await (const item of items){
-//     seedDB(item);
+//<---------axios 로 일정관련 API 하나하나 가져오는방법--------->
+//<---------성능 문제인지 axios 로 get 하는 도중 데이터가 undefined 로 가져와진다...---------->
+// const seedDB = async() =>{
+//     for await (let item of items){
+//         const response = await axios.get(`${url}?serviceKey=${serviceKey}&numOfRows=${numOfRows}&pageNo=${pageNo}&dataFormat=${dataFormat}&implYy=${implYy}&qualgbCd=${item.qualgbcd}&jmCd=${fillZero(String(item.jmcd))}`);
+//         const totalCount = response.data.body.totalCount;
+//         let objInsideObj = [];
+//         for(let i = 0; i < totalCount; i++){
+//             const data = {
+//                 implYy : response.data.body.items[i].implYy,
+//                 implseq : response.data.body.items[i].implSeq,
+//                 dateDescription : response.data.body.items[i].description,
+//                 docRegStartDt : response.data.body.items[i].docRegStartDt,
+//                 docRegEndDt : response.data.body.items[i].docRegEndDt,
+//                 docExamStartDt : response.data.body.items[i].docExamStartDt,
+//                 docExamEndDt : response.data.body.items[i].docExamEndDt,
+//                 docPassDt : response.data.body.items[i].docPassDt,
+//                 pracRegStartDt : response.data.body.items[i].pracRegStartDt,
+//                 pracRegEndDt : response.data.body.items[i].pracRegEndDt,
+//                 pracExamStartDt : response.data.body.items[i].pracExamStartDt,
+//                 pracExamEndDt : response.data.body.items[i].pracExamEndDt,
+//                 pracPassDt : response.data.body.items[i].pracPassDt
+//             };
+//             objInsideObj.push(data);
+//         } 
+//         obj.push(objInsideObj);
+//     }
+//     console.log(obj);
 // }
-// const seedDB = async () =>{
-//     await User.deleteMany({});
-//     await User.insertMany(data);
-//     mongoose.connection.close();
-//     console.log("seeded done!"); 
-// };
+
+
+//<-------종목 이름, 종류만 DB 에 넣어둔 후 일정에 관련된 API는 URL만 DB에 넣는 방법-------->
+//<-------현재 사용중-------->
+const seedDB = (async (item)=>{
+    await GoalElement.deleteMany({});
+    const dataJson = {
+        name : item.jmfldnm,
+        qualgbnm : item.qualgbnm,
+        description : item.description,
+        seriesnm : item.seriesnm,
+        obligfldnm : item.obligfldnm,
+        mdobligfldnm : item.mdobligfldnm,
+        dateUrl : `${url}?serviceKey=${serviceKey}&numOfRows=${numOfRows}&pageNo=${pageNo}&dataFormat=${dataFormat}&implYy=${implYy}&qualgbCd=${item.qualgbcd}&jmCd=${fillZero((item.jmcd).toString())}`,
+        isQnet : true
+    }
+    const newData = new GoalElement(dataJson);
+    await newData.save();
+})
+
+for(const item of items){
+    seedDB(item);
+}
