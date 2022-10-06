9852e3169f9ec1d2647e5b65d6020f09d83d3d98
YoungJune Kang <cerealmaster@naver.com>
2022-10-06 14:59:10 +0000

    goalElement rooute config

--- WEB(BE)/src/controller/newUserInfoFetchingAPI.js
+++ WEB(BE)/src/controller/newUserInfoFetchingAPI.js
@@ -1,11 +1,14 @@
 import User from '../models/user';
+import GoalElement from '../models/goalElement';
 import axios from 'axios';
 import "../env.js";
 import mongoose from 'mongoose';
 import { db_cstring , session_secret , kakao_key , qnet_key} from "../db.js";
-// https 로 데이터 받아오면 validation 에서 막히는듯하다. url 은 왠만하면 http 로 받아오자...!
-// process.env.NODE_TLS_REJECT_UNAUTHORIZED ="0";
+//getCtfInfo, postNewCtfInfo, updateCtfInfo, deleteCtfInfo, getUserGoal, postNewUserGoal, deleteUserGoal
 
-module.exports.getFetchWithQnet = async(req,res,next)=>{
-    res.send('hihi');
+//GET certificate Info from goalElement
+module.exports.getCtfInfo = async(req,res,next)=>{
+    const {id} = req.params;
+    const ctf = await GoalElement.findById(id);
+    res.send(ctf);
 }
