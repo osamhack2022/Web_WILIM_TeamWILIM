32df14dfc188b6945ceb822d9c099a42a71917b6
YoungJune Kang <cerealmaster@naver.com>
2022-10-07 13:49:41 +0000

    backend - getCtfInfo controller added

--- WEB(BE)/src/controller/newUserInfoFetchingAPI.js
+++ WEB(BE)/src/controller/newUserInfoFetchingAPI.js
@@ -6,9 +6,14 @@ import mongoose from 'mongoose';
 import { db_cstring , session_secret , kakao_key , qnet_key} from "../db.js";
 //getCtfInfo, postNewCtfInfo, updateCtfInfo, deleteCtfInfo, getUserGoal, postNewUserGoal, deleteUserGoal
 
+//POST post new certificate Info
+// module.exports.postNewCtfInfo = async(req,res,next)=>{
+// }
+
 //GET certificate Info from goalElement
 module.exports.getCtfInfo = async(req,res,next)=>{
     const {id} = req.params;
     const ctf = await GoalElement.findById(id);
-    res.send(ctf);
+    const response = await axios.get(ctf.dateUrl);
+    res.status(200).json({ctf, date : response.data.body});
 }
