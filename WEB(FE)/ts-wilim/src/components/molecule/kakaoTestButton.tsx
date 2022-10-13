9eba64bd54e62b6be6d5a4624dc49d73e7490b36
Geun-Oh <kandy1002@naver.com>
2022-10-13 13:49:51 +0000

    kakao login logic modified

--- WEB(FE)/ts-wilim/src/components/molecule/kakaoTestButton.tsx
+++ WEB(FE)/ts-wilim/src/components/molecule/kakaoTestButton.tsx
@@ -1,13 +1,11 @@
+import { useDispatch } from "react-redux";
+import { kakaoLogin } from "../../store/asyncThunks/kakaoLogin";
+import { AppThunkDispatch } from "../../store/store";
 import { Button } from "../atom/button";
 
 export const KakaoTestButton = () => {
-    const URL = "https://wilimbackend.tk/userSchemaAPI/login/kakao";
-    const onClick = (e: any) => {
-        e.preventDefault();
-        window.location.href = URL;
-    }
-
+    const dispatch = useDispatch<AppThunkDispatch>();
     return (
-            <Button innerText="카카오로 로그인하기" width="60%" onClick={(e) => onClick(e)} color="#3A1D1D" backgroundColor="#F7E600" hoverColor="#F7E600" />
+            <Button innerText="카카오로 로그인하기" width="60%" onClick={() => dispatch(kakaoLogin())} color="#3A1D1D" backgroundColor="#F7E600" hoverColor="#F7E600" />
     )
 };
