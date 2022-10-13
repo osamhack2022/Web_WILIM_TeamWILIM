df93fb356ce61e627813f6c93c4b69d5c7d99e7d
Geun-Oh <kandy1002@naver.com>
2022-10-13 13:52:39 +0000

    kakao login button mod

--- WEB(FE)/ts-wilim/src/components/molecule/kakaoTestButton.tsx
+++ WEB(FE)/ts-wilim/src/components/molecule/kakaoTestButton.tsx
@@ -1,11 +1,19 @@
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { kakaoLogin } from "../../store/asyncThunks/kakaoLogin";
 import { AppThunkDispatch } from "../../store/store";
 import { Button } from "../atom/button";
 
 export const KakaoTestButton = () => {
+    const navigate = useNavigate();
     const dispatch = useDispatch<AppThunkDispatch>();
     return (
-            <Button innerText="카카오로 로그인하기" width="60%" onClick={() => dispatch(kakaoLogin())} color="#3A1D1D" backgroundColor="#F7E600" hoverColor="#F7E600" />
+        <Button innerText="카카오로 로그인하기" width="60%" onClick={() => {
+            dispatch(kakaoLogin())
+                .then(res => {
+                    if (res.meta.requestStatus === "fulfilled") navigate(`/main`);
+                });
+        }
+        } color="#3A1D1D" backgroundColor="#F7E600" hoverColor="#F7E600" />
     )
 };
