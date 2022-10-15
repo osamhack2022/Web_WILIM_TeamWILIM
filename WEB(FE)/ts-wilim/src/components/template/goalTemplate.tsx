e8dca563c5903355b97281f111de2030e01d823e
Geun-Oh <kandy1002@naver.com>
2022-10-15 14:39:06 +0000

    goal 드롭다운 구현 중

--- WEB(FE)/ts-wilim/src/components/template/goalTemplate.tsx
+++ WEB(FE)/ts-wilim/src/components/template/goalTemplate.tsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import ReactDropdown from "react-dropdown";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { ReducerType } from "../../store/rootReducer";
@@ -13,7 +12,6 @@ import { Text } from "../atom/text";
 import { ExamDate } from "../molecule/examDate";
 import { Title } from "../molecule/title";
 import { BaseStyles } from "../theme";
-import 'react-dropdown/style.css';
 import { AppThunkDispatch } from "../../store/store";
 import { updateUserGoal } from "../../store/asyncThunks/updateUserGoal";
 
@@ -21,24 +19,38 @@ export const GoalTemplate = () => {
     const dispatch = useDispatch();
     const appDispatch = useDispatch<AppThunkDispatch>();
     const isBoxOpen = useSelector((state: ReducerType) => state.toggle.examRound);
+    const { username, goal } = useSelector((state: ReducerType) => state.userInfo);
     const [options, setOptions] = useState<string[]>([]);
     const res = async () => await axios("https://wilimbackend.tk/userGoalElementAPI/ctfInfo").then(res => {
-        for(let i = 0; i < res.data.length; i++) {
+        for (let i = 0; i < res.data.length; i++) {
             setOptions(prev => prev.concat(res.data[i].name));
         }
     });
     useEffect(() => {
         res();
     }, [])
+
     return (
         <Flex flexDirection="column">
             <Title innerText="Goal" />
             <MarginBox marginBottom="2rem" />
             <Box width="calc(100% - 2rem)">
-                <Text innerText="조주기능사" fontSize={BaseStyles.Text.Heading2} />
+                <Text innerText={goal} fontSize={BaseStyles.Text.Heading2} />
             </Box>
             <MarginBox marginBottom="2rem" />
-            <ReactDropdown options={options} value={options[0]} placeholder="Select your goal" />
+            <>
+                {
+                    options.map((item, index) => {
+                        return (
+                            <Box borderRadius="0 0 0 0" width="calc(100% - 2rem)">
+                                <Flex width="100%" justifyContent="flex-start" alignItems="center" key={index}>
+                                    <Text innerText={item} fontSize={BaseStyles.Text.Heading4} onClick={() => appDispatch(updateUserGoal({ username, goalElement: item }))} />
+                                </Flex>
+                            </Box>
+                        )
+                    })
+                }
+            </>
             <MarginBox marginBottom="2rem" />
             <Box width="calc(100% - 2rem)" borderRadius="6px 6px 0 0">
                 <Flex justifyContent="space-between" alignItems="center">
@@ -49,7 +61,7 @@ export const GoalTemplate = () => {
                     </div>
                 </Flex>
             </Box>
-            <Box width="calc(100% - 2rem)" borderRadius="0 0 6px 6px" backgroundColor="#767676" height={isBoxOpen ? "50vh" : "0"} style={{ paddingTop: isBoxOpen ? "1rem": "0", paddingBottom: isBoxOpen ? "1rem": "0" }}>
+            <Box width="calc(100% - 2rem)" borderRadius="0 0 6px 6px" backgroundColor="#767676" height={isBoxOpen ? "50vh" : "0"} style={{ paddingTop: isBoxOpen ? "1rem" : "0", paddingBottom: isBoxOpen ? "1rem" : "0" }}>
                 <Flex flexDirection="column" justifyContent="flex-start" height="100%">
                     <ExamDate examRound="2022년 제 126회차" startAt="20221004" dDay="D - 5" />
                     <ExamDate examRound="2022년 제 127회차" startAt="20221021" dDay="D + 10" />
