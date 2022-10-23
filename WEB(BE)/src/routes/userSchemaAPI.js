3b311b0014e24015ba4cbb4f660e4cc5616c8bf0
Geun-Oh <kandy1002@naver.com>
2022-10-23 15:07:39 +1000

    Update userSchemaAPI.js

--- WEB(BE)/src/routes/userSchemaAPI.js
+++ WEB(BE)/src/routes/userSchemaAPI.js
@@ -18,7 +18,7 @@ router.get('/logout', (req,res)=>{
         if(err){
             throw new ExpressError(404,"오류가 발생했습니다");
         }
-        res.status(200).json({ msg: "로그아웃되었습니다" }); 
+        res.send(`<script type="text/javascript">alert("로그아웃 되었습니다."); window.location.href = "https://3000-geunoh-webwilimteamwili-35mzi13263f.ws-us72.gitpod.io/" </script>`);
     });
 })
 
