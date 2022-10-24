de4933bb6e008e8d75bbc3a03675d82541fd3440
Geun-Oh <kandy1002@naver.com>
2022-10-24 15:24:51 +0000

    백엔드 파일 삭제

--- WEB(BE)/src/db.js
+++
@@ -1,8 +0,0 @@
-export const db_cstring = process.env.DB_CSTRING || 'mongodb+srv://WILIM:wilimadmin@wilim-database.vtlkdho.mongodb.net/?retryWrites=true&w=majority';
-export const session_secret = process.env.SESSION_SECRET || 'WILIMSESSIONSECRETKEY';
-export const kakao_key = process.env.KAKAO_KEY || "924213ef9bf44dea96f40c16d7aab8f3";
-export const qnet_key = process.env.QNET_KEY;
-export const naver_client_id = process.env.NAVER_CLIENT_ID || "BzGElduCaHreHASzKelv";
-export const naver_client_secret = process.env.NAVER_CLIENT_SECRET || "qgRyTV0dyK";
-export const mail_id = process.env.MAIL_ID || "sandboxe201ae1fbd514dcf83193eee21e8e568.mailgun.org";
-export const mail_key = process.env.MAIL_KEY || "key-a1a15d9d15fc2e73c9c25d469cb679a0";
