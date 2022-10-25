37482606a9e32b498b4a1247a73b61b1b32771e6
Geun-Oh <kandy1002@naver.com>
2022-10-25 14:36:40 +0000

    커뮤니티에 자신의 글 확인하는 페이지 추가

--- WEB(FE)/ts-wilim/src/components/template/communityTemplate.tsx
+++ WEB(FE)/ts-wilim/src/components/template/communityTemplate.tsx
@@ -1,22 +1,32 @@
+import { useDispatch } from "react-redux"
 import { useSelector } from "react-redux"
 import { ReducerType } from "../../store/rootReducer"
+import { myPostToggle } from "../../store/slices/toggleSlice"
 import { Flex } from "../atom/flex"
 import { MarginBox } from "../atom/marginBox"
+import { Text } from "../atom/text"
 import { Title } from "../molecule/title"
 import { NewPostCard } from "../organism/newPostCard"
 import { PostCard } from "../organism/postCard"
+import { BaseStyles } from "../theme"
 
 export const CommunityTemplate = () => {
-    const postCardList = useSelector((state: ReducerType) => state.post);
+    const { name } = useSelector((state: ReducerType) => state.userGoal);
+    const { _id } = useSelector((state: ReducerType) => state.userInfo);
+    const myPost = useSelector((state: ReducerType) => state.toggle.myPost);
+    const postList = useSelector((state: ReducerType) => state.post);
+    const dispatch = useDispatch();
+    const postCardList = myPost ? [ ...postList.postList.filter(x => x.hashtags[0] === `#${name}`), ...postList.postList.filter(x => x.hashtags[0] !== `#${name}`) ] : [ ...postList.postList.filter(x => x.owner._id === _id) ];
     return (
         <Flex flexDirection="column" alignItems="center">
             <Title innerText="Community" />
+            <Text innerText="My Posts" fontSize={BaseStyles.Text.Heading4} color={myPost ? "#FF8946" : "#BBBBBB"} onClick={() => dispatch(myPostToggle())} />
             <MarginBox marginBottom="2rem" />
             <NewPostCard />
             <MarginBox marginBottom="1.5rem" />
             <>
                 {
-                    postCardList && postCardList.postList.map((item, index) => (
+                    postCardList && postCardList.map((item, index) => (
                         <>
                             <PostCard _id={item._id} title={item.title} hashtags={item.hashtags} content={item.content} comments={item.comments} key={index} owner={item.owner} likedUsers={item.likedUsers} />
                             <MarginBox marginBottom="1.5rem" />
