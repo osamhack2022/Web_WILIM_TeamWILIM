7d96e1a62bf710800465e376983e776b38cd244e
Geun-Oh <kandy1002@naver.com>
2022-10-28 18:05:54 +1000

    Update originURL.ts

--- WEB(FE)/ts-wilim/src/utils/originURL.ts
+++ WEB(FE)/ts-wilim/src/utils/originURL.ts
@@ -1,5 +1,5 @@
-// const originURL = "https://wilimbackend.tk";
+const originURL = "https://wilimbackend.tk";
 
-const originURL = "https://5000-geunoh-webwilimteamwili-7rjej01dmzm.ws-us73.gitpod.io";
+// const originURL = "https://5000-geunoh-webwilimteamwili-7rjej01dmzm.ws-us73.gitpod.io";
 
 export default originURL;
